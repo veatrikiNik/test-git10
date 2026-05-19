1c97bc6cf619840a2cb33e30f752e90a0878b980
veatrikiNik <beatricenik12@gmail.com>
2026-05-19 18:42:22 +0300

    chore: Update greeting message and modify gitignore

--- src/gr/aueb/cf/Main.java
+++ src/gr/aueb/cf/Main.java
@@ -2,6 +2,6 @@ package gr.aueb.cf;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
     }
 }
